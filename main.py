75ccdbe2b9867701bb4fbfd84066abaf52b0c2cf
ruhishahh <106028647+ruhishahh@users.noreply.github.com>
2022-10-12 11:51:23 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from bankingweb import createApp
+
+app = createApp()
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
+##extras
+'''
+if we end up wanting to add images, javascript or css, skip to 29 in vid
+'''
