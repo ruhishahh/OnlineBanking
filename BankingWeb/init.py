120e97d097608a22a33c35d1fcafdec10b2b245d
ruhishahh <106028647+ruhishahh@users.noreply.github.com>
2022-10-12 11:54:30 -0400

    Create init.py

---
+++ BankingWeb/init.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+
+
+def createApp():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'spongebobsquarepants'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    from .auth import auth
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Transaction
+    createDataBase(app)
+    
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+
+    return app
+    
+
+def createDataBase(app):
